963e71ea4371dcf0086016f6313f1aa6ed7aecbc
Shanmukha <Shanmukha2491@gmail.com>
2024-11-26 22:11:45 +0530

    Added schema for different roles

--- models/schedule.go
+++ models/schedule.go
@@ -1,1 +1,13 @@
 package model
+
+import "time"
+
+
+type Schedule struct{
+	Id int 
+	Class_Id int
+	Faculty_Id int 
+	Day string
+	StartTime time.Time
+	EndTime time.Time
+}
--- models/student.go
+++ models/student.go
@@ -1,1 +1,12 @@
 package model
+
+
+type Student struct{
+	Id int 
+	Name string
+	Email string
+	Password string
+	Enrollment_No string
+	Class_Id string
+	Create_At string
+}
